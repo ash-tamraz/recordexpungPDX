b91e00f21a69ab685bb73a3a5ef0504dc347cfba
Adam Holm <asorholm@email.arizona.edu>
2019-04-11 11:06:09 -0700

    Progress on search endpoint and search endpoint testing file

--- src/backend/expungeservice/endpoints/search.py
+++ src/backend/expungeservice/endpoints/search.py
@@ -1,19 +1,39 @@
-"""Flask endpoint for searching the remote site. The web app will send `POST` 
+"""Flask endpoint for searching the remote site. The web app will send `POST`
 requests to this endpoint with search parameters in the body of the request.
 This endpoint will then send a request to the remote site.
 
 Data format TBD.
 
+Look in slack channel for remote site login
+
 """
 
 from flask.views import MethodView
-from flask import request, current_app
+from flask import request, current_app, jsonify, abort
 
+# idea: create custom class based on MethodView which checks headers, etc.
+
+# post request will need authentication header from auth.py endpoint
+# Arun wrote decorator for authentication
 class SearchQuery(MethodView):
   def post(self):
     data = request.get_json()
-    return "data received" 
 
+    response_data = {
+      'data': None
+    }
+
+    # Response for empty query
+    if data == None or data == {}:
+      abort(400)
+
+    # Testing receiving basic POST request
+    # Expecting incoming POST request to have three fields, First Name,
+    # Last Name, and Date of Birth (DoB)
+    if data['first_name'] and data['last_name'] and data['dob']:
+      response_data['data'] = data['first_name'] + data['last_name'] + data['dob']
+
+    return jsonify(response_data), 201
 
 def register(app):
   # app.add_url_rules will go here
--- src/backend/tests/endpoints/test_search.py
+++ src/backend/tests/endpoints/test_search.py
@@ -1,4 +1,4 @@
-"""Test file for Flask endpoint for api/search, 
+"""Test file for Flask endpoint for api/search,
 `src/backend/expungeservice/endpoints/search.py`
 
 Uses `pytest` module for testing.
@@ -7,7 +7,7 @@ Uses `pytest` module for testing.
 
 import pytest
 
-from flask import current_app
+from flask import current_app, jsonify
 
 import expungeservice
 from expungeservice.endpoints import search
@@ -23,15 +23,51 @@ def client(app):
 def test_basic(client):
   """Super basic test to see if app is working at all. Will just test response
   from `hello.py` endpoint.
- 
+
   @param client: app.test_client()
   """
   response = client.get('/hello')
   assert(response.data == b'Hello, world!')
 
+def test_empty_post(client):
+  """Testing error response from endpoint for POST request of `None`.
+  @param client: app.test_client()
+  """
+  response = client.post('api/v0.1/search', None)
+  assert(response.status_code == 400)
+
+def test_empty_obj_post(client):
+  """Testing error response from endpoint for empty POST request.
+  @param client: app.test_client()
+  """
+  response = client.post('api/v0.1/search', json={})
+  assert(response.status_code == 400)
+
 def test_basic_post(client):
+  """Testing super basic response to make sure I'm understanding Flask's
+  POST request functionality correctly.
+  @param client: app.test_client()
+  """
   response = client.post('api/v0.1/search', json={
-    'query': 'hello world',
+    'first_name': 'foo',
+    'last_name': 'bar',
+    'dob': 'baz'
   })
-  assert(response.status_code == 200) 
-  assert(response.data == b"data received") 
+  endpoint_response = response.get_json()
+  assert(response.status_code == 201)
+  assert(endpoint_response['data'] == 'foobarbaz')
+
+def test_basic_post_w_field_contents(client):
+  """Testing POST request with fields' contents filled out
+  @param client: app.test_client()
+  """
+  response = client.post('api/v0.1/search', json={
+    'first_name': 'Cool',
+    'last_name': 'Guy',
+    'dob': '00/00/0000'
+  })
+  endpoint_response = response.get_json()
+  assert(response.status_code == 201)
+  assert(endpoint_response['data'] == 'CoolGuy00/00/0000')
+
+
