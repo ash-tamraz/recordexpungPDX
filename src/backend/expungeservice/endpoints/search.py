84a6a3d9a1784e4ac60a5e87f1b7b95b83768dd6
Adam Holm <asorholm@email.arizona.edu>
2019-07-06 19:06:23 -0700

    Trying to get requests_mock to play nice with test_search

--- src/backend/expungeservice/endpoints/search.py
+++ src/backend/expungeservice/endpoints/search.py
@@ -13,57 +13,47 @@ from flask.views import MethodView
 from flask import request, current_app, jsonify, abort
 
 import expungeservice
-from .auth import auth_required
+from expungeservice.endpoints.auth import user_auth_required
 from expungeservice.crawler.crawler import Crawler
 
-# idea: create custom class based on MethodView which checks headers, etc.
-
-# post request will need authentication header from auth.py endpoint
-# Arun wrote decorator for authentication
 class SearchQuery(MethodView):
-  def __init__(self):
-      self.query = None
-      self.login_info = None
-      self.search_results = None
+    def __init__(self):
+        self.query = None
+        self.login_info = None
+        self.search_results = None
+        self.request = None
 
-  @auth_required
-  def post(self):
-    data = request.get_json() or None
+    @user_auth_required
+    def post(self):
+        data = request.get_json() or None
 
-    # Response for empty query
-    if data == None or data == {}:
-      abort(400)
+        # Response for empty query
+        if data == None or data == {}:
+            abort(400)
 
-    # Data good. Passing to `crawler.py`
-    self.query = json.loads(data)
+        # Data good. Passing to `crawler.py`
+        self.request = data
 
-#    self.fill_login_info()
-    # Testing receiving basic POST request
-    # Expecting incoming POST request to have three fields, First Name,
-    # Last Name, and Date of Birth (DoB)
-    length = self.pass_to_crawler()
-    # Send results to expunger
-    return length, 201
+        crawler_response = self.pass_to_crawler()
 
-  def pass_to_crawler(self):
-    crawl = Crawler()
-    # First need to log in
-    crawler_login = crawl.login('username', 'password')
-#    if crawler_login:
-#      crawl.search(self.query['first_name'],
-#              self.query['last_name'], 
-#              'fiz', 
-#              self.query['dob'])
-    return type(crawler_login)
+        # Returning this for testing purposes
+        return jsonify(crawler_response), 201
 
-#  def fill_login_info(self):
-#    self.login_info = {
-#            'username': self.query['username'] or None,
-#            'password': self.query['password'] or None
-#            }
+    def pass_to_crawler(self):
+        crawl = Crawler()
+
+        # First need to log in
+
+        # Can get user data from object in `self.request`, but this matches
+        # user info provided to Crawler in CrawlerFactory class
+        crawler_login = crawl.login('username', 'password')
+
+        # Search logic, TBD
+
+        return crawler_login
 
 def register(app):
-  # app.add_url_rules will go here
-  app.add_url_rule('/api/v0.1/search', view_func=SearchQuery.as_view('search'))
+    # app.add_url_rules will go here
+    app.add_url_rule('/api/v0.1/search', view_func=SearchQuery.as_view('search'))
 
 
--- src/backend/tests/endpoints/test_search.py
+++ src/backend/tests/endpoints/test_search.py
@@ -10,146 +10,226 @@ import jwt
 import pytest
 import json
 
-"""Modules borrowed from Nick's test file `test_crawler.py` for testing 
+"""Modules borrowed from Nick's test file `test_crawler.py` for testing
 response from Crawler's search method
 """
+from expungeservice.crawler.crawler import Crawler
+from expungeservice.crawler.request import URL
 from tests.fixtures.post_login_page import PostLoginPage
 from tests.fixtures.search_page_response import SearchPageResponse
 from tests.fixtures.john_doe import JohnDoe
 from tests.fixtures.case_details import CaseDetails
 
-from flask import current_app, jsonify
+from flask import jsonify, current_app, g, request
+from expungeservice.database import user
+from werkzeug.security import generate_password_hash
 
 import expungeservice
 from expungeservice.endpoints import search
 
-@pytest.fixture(scope='module')
-def app():
-  return expungeservice.create_app('development')
+class TestSearch(unittest.TestCase):
 
-@pytest.fixture(scope='module')
-def client(app):
-  return app.test_client()
+    def setUp(self):
+        self.email = 'pytest_user@auth_test.com'
+        self.password = 'pytest_password'
+        self.hashed_password = generate_password_hash(self.password)
 
-"""Borrowing some testing variables from Arun's `test_auth.py`
-"""
-username = 'test_user'
-password = 'test_password'
-email = 'test_user@test.com'
+        # API endpoint paths
+        self.search_path = '/api/v0.1/search'
+        self.auth_path = '/api/v0.1/auth_token'
+        self.users_path = '/api/v0.1/users'
 
-search_path = 'api/v0.1/search'
+        # Mocker variables
+        self.crawler = Crawler()
+        self.mocker_base_url = 'https://publicaccess.courts.oregon.gov/PublicAccessLogin/'
 
-def create_user(client, username, password, email):
-    return client.post('api/v0.1/users', json={
-        'username': username,
-        'password': password,
-        'email address': email,
-    })
+        self.admin_email = 'pytest_admin@auth_test.com'
+        self.admin_password = 'pytest_password_admin'
+        self.hashed_admin_password = generate_password_hash(self.admin_password)
 
-def get_auth_token(client, username, password):
-    return client.get('/api/v0.1/auth_token', json={
-        'username': username,
-        'password': password,
-    })
+        self.app = expungeservice.create_app('development')
+        self.client = self.app.test_client()
 
-def create_post_obj(client, username, password):
-  """Helper function to create dict objects for sending to search.py via
-  POST requests
-  """
-  token = get_auth_token(client, username, password)
-  query_obj = dict({('first_name', 'foo'),
-              ('last_name', 'bar'),
-              ('dob', 'baz')})
-  return json.dumps(query_obj)
+        with self.app.app_context():
+            expungeservice.request.before()
 
-def create_header_obj(client, username, password):
-  token = get_auth_token(client, username, password)
-  return dict({('Authorization', 'Bearer {}'.format(token.get_json()['auth_token']))})
+            self.db_cleanup()
+            user.create_user(g.database, self.email, self.hashed_password, False)
+            user.create_user(g.database, self.admin_email, self.hashed_admin_password, True)
+            expungeservice.request.teardown(None)
 
-def test_create_user(client):
-    response = create_user(client, username, password, email)
-    assert(response.status_code == 201)
+    def tearDown(self):
+        with self.app.app_context():
+            expungeservice.request.before()
 
-    data = response.get_json()
-    assert(data['username'] == username)
-    assert(data['email address'] == email)
+            self.db_cleanup()
+            expungeservice.request.teardown(None)
 
-def test_basic(client):
-  """Super basic test to see if app is working at all. Will just test response
-  from `hello.py` endpoint.
+    def db_cleanup(self):
 
-  @param client: app.test_client()
-  """
-  response = client.get('/hello')
-  assert(response.data == b'Hello, world!')
+        cleanup_query = """DELETE FROM users where email like %(pattern)s;"""
+        g.database.cursor.execute(cleanup_query, {"pattern":"%pytest%"})
+        g.database.connection.commit()
 
-def test_empty_post(client):
-  """Testing error response from endpoint for POST request of `None`.
-  @param client: app.test_client()
-  """
-  response = client.post('api/v0.1/search', None)
-  assert(response.status_code == 401)
+    def _get_auth_token(self, email, password):
+        return self.client.get(self.auth_path, json={
+            'email': email,
+            'password': password,
+        })
 
-def test_empty_post_wrong_request_method(client):
-  """Testing error response from endpoint for POST request of `None`.
-  @param client: app.test_client()
-  """
-  response = client.get('api/v0.1/search', None)
-  assert(response.status_code == 405)
 
-def test_empty_post_w_valid_token(client):
-  """Testing error response from endpoint for POST request of `None`.
-  @param client: app.test_client()
-  """
-  token = get_auth_token(client, 'test_user', 'test_password')
-  response = client.post('/api/v0.1/search', headers={
-        'Authorization': 'Bearer {}'.format(token.get_json()['auth_token'])
-    })
-  assert(response.status_code == 400)
+    def _create_post_obj(self):
+        """Helper function to create dict objects for sending to search.py via
+        POST requests
+        """
 
-def test_empty_obj_post_wo_valid_token(client):
-  """Testing error response from endpoint for empty POST request.
-  @param client: app.test_client()
-  """
-  response = client.post(search_path, headers={}, json={})
-  assert(response.status_code == 401)
+        query_obj = dict({('first_name', 'foo'),
+            ('last_name', 'bar'),
+            ('dob', 'baz')})
 
-def test_empty_obj_post_w_valid_token(client):
-  """Testing error response from endpoint for empty POST request.
-  @param client: app.test_client()
-  """
-  header_obj = create_header_obj(client, username, password)
-  response = client.post(search_path, headers=header_obj, json={})
-  assert(response.status_code == 400)
+        user_obj = dict({
+            ('email', self.email),
+            ('password', self.password)
+        })
 
-#"""I'm not sure about structure or content of object received from the POST
-#   request at this point. I'm assuming we will at least have fields for the
-#   remote site's search fields. Will we send the auth_token to ensure that 
-#   the sender is permitted to search the database?
+        post_obj = dict({
+            'query' : query_obj,
+            'user' : user_obj
+        })
+
+        return post_obj
+
+    def _create_user(self):
+
+        new_email = "pytest_create_user@endpoint_test.com"
+        new_password = "new_password"
+        new_hashed_password = generate_password_hash(new_password)
+
+
+        auth_response = self._get_auth_token(self.admin_email, self.admin_password)
+
+        response = self.client.post(self.users_path, headers={
+            'Authorization': 'Bearer {}'.format(auth_response.get_json()['auth_token'])},
+                json = {'email':new_email,
+                        'password': new_password,
+                        'admin': True})
+
+
+
+        return response
+
+    def test_create_user_success(self):
+
+        new_email = "pytest_create_user@endpoint_test.com"
+        new_password = "new_password"
+        new_hashed_password = generate_password_hash(new_password)
+
+
+        auth_response = self._get_auth_token(self.admin_email, self.admin_password)
+
+        response = self.client.post(self.users_path, headers={
+            'Authorization': 'Bearer {}'.format(auth_response.get_json()['auth_token'])},
+                json = {'email':new_email,
+                        'password': new_password,
+                        'admin': True})
+
+
+        assert(response.status_code == 201)
+
+        data = response.get_json()
+        assert data['email'] == new_email
+        assert data['admin'] == True
+        assert data['timestamp']
+
+    def test_search_empty_request(self):
+        new_email = "pytest_create_user@endpoint_test.com"
+        new_password = "new_password"
+        new_hashed_password = generate_password_hash(new_password)
+
+
+        auth_response = self._get_auth_token(self.admin_email, self.admin_password)
+        post_obj = self._create_post_obj()
+        header_obj = {
+            'Authorization': 'Bearer {}'.format(auth_response.get_json()['auth_token'])
+        }
+
+        with requests_mock.Mocker() as m:
+            m.post(self.mocker_base_url + 'Search.aspx?ID=100', [{'text': SearchPageResponse.RESPONSE}, {'text': JohnDoe.BLANK_RECORD}])
+            response = self.client.post(self.search_path, headers=header_obj, json=post_obj)
+            print("XXXresponse: ",response.get_json())
+#            print(response)
+        assert(False)
+#            for key, value in self.crawler.result.cases.items():
+#                m.get("{}{}{}".format(base_url, 'CaseDetail.aspx?CaseID=', key), text=value)
+#
+#            self.crawler.search('John', 'Doe')
+##    self.crawler.search('John', 'Doe')
+
+#        response = self.client.post('/api/v0.1/search', headers={
+#            'Authorization': 'Bearer {}'.format(auth_response.get_json()['auth_token'])},
+#                json = {'email':new_email,
+#                        'password': new_password,
+#                        'admin': True})
+#
+#        assert(response.status_code == 201)
+
+#@pytest.fixture(scope='module')
+#def app():
+#  return expungeservice.create_app('development')
+#
+#@pytest.fixture(scope='module')
+#def client(app):
+#  return app.test_client()
+#
+#"""Borrowing some testing variables from Arun's `test_auth.py`
 #"""
-def test_basic_post_w_valid_auth_token(client):
-#  """Testing super basic response to make sure I'm understanding Flask's
-#  POST request functionality correctly.
-#  @param client: app.test_client()
-#  """
-  base_url = 'https://publicaccess.courts.oregon.gov/PublicAccessLogin/'
-  with requests_mock.Mocker() as m:
-    m.post(base_url + 'Search.aspx?ID=100', [{'text': SearchPageResponse.RESPONSE},
-    {'text': JohnDoe.BLANK_RECORD}])
-    post_obj = create_post_obj(client, username, password)
-    header_obj = create_header_obj(client, username, password)
-    response = client.post(search_path, headers=header_obj, json=post_obj)
-#    self.crawler.search('John', 'Doe')
+#email = 'pytest_create_user@endpoint_test.com'
+#password = 'new_password'
+#hashed_password = generate_password_hash(password)
 #
-#  assert len(self.crawler.result.cases) == 0
-
-#  post_obj = create_post_obj(client, username, password)
-#  header_obj = create_header_obj(client, username, password)
-#  response = client.post(search_path, headers=header_obj, json=post_obj)
-#  assert(response.status_code == 201)
-##  assert(json.loads(response.get_json())['first_name'] == 'foo')
-#  assert("Crawler search passed foobarfizbaz" == response.get_json())
+#admin_email = 'pytest_admin@auth_test.com'
+#admin_password = 'pytest_password_admin'
+#hashed_admin_password = generate_password_hash(admin_password)
 #
-
-
-
+#search_path = 'api/v0.1/search'
+#
+#def test_create_user_success(client):
+#            expungeservice.request.before()
+#
+#            user.create_user(g.database, email, hashed_password, False)
+#            user.create_user(g.database, admin_email, hashed_admin_password, True)
+#
+#    get_auth_response = self.get_auth_token(self.admin_email, self.admin_password)
+#
+#    response = self.client.post('/api/v0.1/users', headers={
+#        'Authorization': 'Bearer {}'.format(get_auth_response.get_json()['auth_token'])},
+#            json = {'email':new_email,
+#                    'password': new_password,
+#                    'admin': True})
+#
+#
+#    assert(response.status_code == 201)
+#
+#    data = response.get_json()
+#    assert data['email'] == new_email
+#    assert data['admin'] == True
+#    assert data['timestamp']
+#
+#
+#def test_basic_post_w_valid_auth_token(client):
+##  """Testing super basic response to make sure I'm understanding Flask's
+##  POST request functionality correctly.
+##  @param client: app.test_client()
+##  """
+#  base_url = 'https://publicaccess.courts.oregon.gov/PublicAccessLogin/'
+#  with requests_mock.Mocker() as m:
+#    m.post(base_url + 'Search.aspx?ID=100', [{'text': SearchPageResponse.RESPONSE},
+#    {'text': JohnDoe.BLANK_RECORD}])
+#    post_obj = create_post_obj(client, username, password)
+#    header_obj = create_header_obj(client, username, password)
+#    response = client.post(search_path, headers=header_obj, json=post_obj)
+##    self.crawler.search('John', 'Doe')
+##
+##  assert len(self.crawler.result.cases) == 0
+#
+#
